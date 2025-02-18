730444966fabe1ed8ac920893f5eb6f24beb8326
Ezechiel <ef@bid.on>
2025-02-18 16:11:36 +0100

    9VHiu9ku/4-en-tant-que-joueur-je-veux-voir-la-balle-rebondir-sur-les-bord-de-la-fenêtre

--- rebondir.py
+++ rebondir.py
@@ -5,7 +5,7 @@ import time
 tk = Tk()
 tk.resizable(0, 0)
 tk.wm_attributes("-topmost", 1)
-canvas = Canvas(tk, width=500, height=500, bd=0, highlightthickness=0)
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
 
@@ -14,24 +14,34 @@ class Ball:
         self.canvas = canvas
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
-        starts = [-0.005, -0.0025, 0, 0.0025, 0.05]
+        starts = [-3, -2, -1, 1, 2, 3]
         random.shuffle(starts)
         self.x = starts[0]
-        self.y = -0.0025
+        self.y = -0.3
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
+
+class Rectangle:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0, 10, 25, 25, fill=color)
         
     def draw(self):
-        pass
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <= 0:
+            self.y = 3
+        if pos[3] >= self.canvas_height:
+            self.y = -3
+        if pos[0] <= 0:
+            self.x = 3
+        if pos[2] >= self.canvas_width:
+            self.x = -3
         
 ball = Ball(canvas, 'red')
 
-def move():
-    canvas.move(ball.id,1,0) #fonction pour deplacer l'objet
-
 while 1:
     ball.draw()
-    move()
-    tk.update()
     tk.update_idletasks()
-    time.sleep(0.005)
+    tk.update()
+    time.sleep(0.01)
