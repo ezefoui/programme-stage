7cea4254086a238ecb3624af773afb88b7a89668
Ezechiel <ef@bid.on>
2025-02-18 16:46:39 +0100

    /36iMIH0w/7-en-tant-que-joueur-je-veux-pouvoir-déplacer-la-raquette-à-laide-des-flêches-gauche-et-droite

--- rebondir.py
+++ rebondir.py
@@ -8,7 +8,6 @@ tk.wm_attributes("-topmost", 1)
 canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
-
 class Ball:
     def __init__(self, canvas, color):
         self.canvas = canvas
@@ -21,11 +20,6 @@ class Ball:
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
 
-class Rectangle:
-    def __init__(self, canvas, color):
-        self.canvas = canvas
-        self.id = canvas.create_rectangle(0, 10, 25, 25, fill=color)
-        
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
@@ -37,11 +31,41 @@ class Rectangle:
             self.x = 3
         if pos[2] >= self.canvas_width:
             self.x = -3
+
+class Rectangle:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0, 0, 50, 15, width=0, fill=color)
+        self.canvas.move(self.id, 350, 250)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        # Lier les touches du clavier pour contrôler le rectangle
+        self.canvas.bind_all('<Left>', self.move_left)
+        self.canvas.bind_all('<Right>', self.move_right)
+    
+    def draw(self):
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        # Empêcher le rectangle de sortir des bords
+        if pos[0] <= 0:
+            self.x = 0
+        elif pos[2] >= self.canvas_width:
+            self.x = 0
+
+    def move_left(self, event):
+        self.x = -3  # Vitesse à gauche
+
+    def move_right(self, event):
+        self.x = 3  # Vitesse à droite
+
         
 ball = Ball(canvas, 'red')
+rectangle = Rectangle(canvas, 'blue')
+
 
 while 1:
     ball.draw()
+    rectangle.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
